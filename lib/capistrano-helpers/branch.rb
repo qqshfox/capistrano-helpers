3fc50e180560d81b457416a06e8f07960f6ec87d
Hanfei Shen <qqshfox@gmail.com>
2012-08-24 15:24:15 +0800

    add default branch HEAD

--- lib/capistrano-helpers/branch.rb
+++ lib/capistrano-helpers/branch.rb
@@ -5,7 +5,8 @@ CapistranoHelpers.with_configuration do
   desc "Ensure that a branch has been selected."
   task :set_branch do
     if !exists?(:branch)
-      set(:branch, Capistrano::CLI.ui.ask("Which tag/branch/commit? "))
+      input = Capistrano::CLI.ui.ask("Which tag/branch/commit? [HEAD] ")
+      set(:branch, input && !input.empty? || "HEAD")
     end
   end
 
